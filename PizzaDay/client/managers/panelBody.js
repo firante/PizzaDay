9c014d8a12ead06e683965bca2ec4b8d8ea6078e
Mykhaylo <misha.fedorovych@gmail.com>
2016-04-15 10:01:26 +0300

    'App1'

--- PizzaDay/client/managers/panelBody.js
+++ PizzaDay/client/managers/panelBody.js
@@ -1,6 +1,38 @@
 Template.panelBody.helpers({
-  employers: () => {
-    Meteor.subscribe('allEmployers');
-    return employers.find().fetch();
+  employers: (id) => {
+    console.log(id)
+    Meteor.subscribe('pizzaDay');
+    return pizzaDay.find().fetch();
+  },
+
+  currUserCheck: (userFirst, userLast) => {
+    if(userFirst + ' ' + userLast === Meteor.users.find().fetch()[0].profile.name) {
+      return true
+    } else {
+      return false;
+    }
+  }
+});
+
+
+Template.panelBody.events({
+  'click #successUser': (e, template) => {
+    var employ = template.find('#userName').innerText;
+    var idPizzaDay = template.find('#employersList').getAttribute('value');
+    Meteor.subscribe('pizzaDay');
+    pizzaDay.update(
+      {_id: idPizzaDay},
+      {$push: {'successEmpl': employ}}
+    );
+  },
+
+  'click #declineUser': (e, template) => {
+    var employ = template.find('#userName').innerText;
+    var idPizzaDay = template.find('#employersList').getAttribute('value');
+    Meteor.subscribe('pizzaDay');
+    pizzaDay.update(
+      {_id: idPizzaDay},
+      {$pull: {allEmploy: [employ]}}
+    );
   }
 });
